ebfda7a1f9d3544f1fea7670c492a51a795dcd42
Biman Kumar Das <daskumarbiman2020@gmail.com>
2024-01-12 13:12:58 +0530

    sign test02

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,46 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Navs from "./components/basics/Navs"
 import Profile from "./pages/Profile";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Header from "./components/basics/Header";
 import AddPage from "./pages/AddPage";
 import { SignOutButton, SignInButton, SignedIn, SignedOut } from "@clerk/clerk-react"
+import { useUser,useAuth } from "@clerk/clerk-react"
 
-const isLogin=true;
+const loggedIn = false;
+
 function App() {
   return (
     <>
       <BrowserRouter>
         {
-          
-          isLogin?
+          <>
+          <SignedIn>
             <div className="flex flex-col h-screen">
               <Header />
               <Routes>
-                
+                <>
                   <Route path="/" element={<Home />} />
                   <Route path="/addClass/student" element={<AddPage teacher={false} />} />
                   <Route path="/addClass/student/:code" element={<AddPage teacher={false} />} />
                   <Route path="/addClass/teacher" element={<AddPage teacher={true} />} />
                   <Route path="/addClass/teacher/:code" element={<AddPage teacher={true} t={1} />} />
                   <Route path="/profile" element={<Profile />} />
-                
+                </>
               </Routes>
               <Navs />
             </div> 
-         
+            </SignedIn>
+            <SignedOut>
             
-            
-             
-             : <Routes>
-                <Route path="*" element={<SignInButton />} />
+              <Routes>
+                <Route path="/" element={<SignInButton />} />
               </Routes>
             
-            
-          
-         
+            </SignedOut>
+            </>
         }
       </BrowserRouter>
-       
     </>
   )
 }
